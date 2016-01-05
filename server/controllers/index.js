343f389691cde6f8e19fdc7cc3855aae5ac14ecf
radelmann <robadelmann@gmail.com>
2016-01-04 17:00:22 -0800

    added server side models class, abstracted from controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,13 +1,4 @@
-var fs = require('fs');
-var Throttle = require('throttle');
-var probe = require('node-ffprobe'); // -> https://github.com/ListenerApproved/node-ffprobe
-
-var mimeTypes = {
-  '.js': 'text/javascript',
-  '.html': 'text/html',
-  '.css': 'text/css',
-  '.mp3': 'audio/mpeg3'
-};
+var models = require('../models');
 
 var mediaDir = '/../media/';
 var mediaFile = 'Gravity.mp3';
@@ -16,121 +7,38 @@ var nextMediaFile = 'Get_Down_Test_1.mp3';
 var mediaFilePath = __dirname + mediaDir + mediaFile;
 var nextFilePath = __dirname + mediaDir + nextMediaFile;
 
-var connectedClients = [];
-var currentStream;
-
-//stream helpers
-var startStream = function(req, res, file, clients, callback) {
-  probe(file, function(err, data) {
-    if (err) {
-      callback(err);
-    }
-
-    var bit_rate = data.format.bit_rate;
-    console.log(data);
-
-    var stream = fs.createReadStream(file);
-
-    //thottle stream reading to match file bit rate
-    var unthrottle = Throttle(stream, (bit_rate / 10) * 1.4);
-
-    stream.on('data', function(data) {
-      //push data to all connected clients
-      clients.forEach(function(client) {
-        console.log('writing data for client');
-        client.write(data);
-      });
-    });
-
-    res.sendStatus(200).end();
-    callback(null, stream);
-  });
-}
-
-var endStream = function(stream, callback) {
-  stream.on('close', function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      callback();
-    }
-  });
-
-  stream.destroy();
-}
-
 module.exports = {
 
   stream: {
     stream: function(req, res) {
-
-      startStream(req, res, mediaFilePath, connectedClients, function(err, stream) {
-        if (err) {
-          console.log(err);
-        } else {
-          currentStream = stream;
-        }
-      });
+      models.stream.start(req, res, mediaFilePath);
     },
 
     pause: function(req, res) {
+      //todo
       res.sendStatus(200).end();
     },
 
     resume: function(req, res) {
+      //todo
       res.sendStatus(200).end();
     },
 
     next: function(req, res) {
-      endStream(currentStream, function() {
-
-        startStream(req, res, nextFilePath, connectedClients, function(err, stream) {
-          if (err) {
-            console.log(err);
-          } else {
-            currentStream = stream;
-          }
-        });
+      models.stream.end(function() {
+        models.stream.start(req, res, nextFilePath);
       });
-
     },
 
     addClient: function(req, res) {
-      res.writeHead(200, {
-        'Content-Type': 'audio/mpeg',
-        'Transfer-Encoding': 'chunked'
-      });
-
-      req.on("close", function() {
-        var index = connectedClients.indexOf(res);
-        connectedClients.splice(index, 1);
-        console.log('request closed - remove response from global array');
-        res.end();
-      });
-
-      connectedClients.push(res);
-
-      console.log('client connected - add response to global array');
+      models.client.connect(req, res);
     }
   },
 
   media: {
     get: function(req, res) {
       //return json list of files in media folder
-      fs.readdir(__dirname + mediaDir, function(err, files) {
-        if (err) {
-          console.log(err);
-          throw (err);
-        } else {
-          var ret = [];
-          files.forEach(function(file) {
-            var fileObj = {};
-            fileObj.fileName = file;
-            ret.push(fileObj);
-          });
-          res.json(ret).end();
-        }
-      });
+      models.media.get(req, res);
     }
   }
 };
---
+++ server/models/index.js
@@ -0,0 +1,107 @@
+var fs = require('fs');
+var Throttle = require('throttle');
+var probe = require('node-ffprobe'); // -> https://github.com/ListenerApproved/node-ffprobe
+
+var mimeTypes = {
+  '.js': 'text/javascript',
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.mp3': 'audio/mpeg3'
+};
+
+//local file paths
+var mediaDir = '/../media/';
+var mediaFile = 'Gravity.mp3';
+var nextMediaFile = 'Get_Down_Test_1.mp3';
+
+var mediaFilePath = __dirname + mediaDir + mediaFile;
+var nextFilePath = __dirname + mediaDir + nextMediaFile;
+
+//todo -- global vars where to store these ??? 
+var globals = {};
+globals.clients = [];
+globals.currentStream;
+
+module.exports = {
+
+  stream: {
+    start: function(req, res, file, callback) {
+      probe(file, function(err, data) {
+        if (err) {
+          callback(err);
+        }
+
+        var bit_rate = data.format.bit_rate;
+        console.log(data);
+
+        var stream = fs.createReadStream(file);
+
+        //thottle stream reading to match file bit rate
+        var unthrottle = Throttle(stream, (bit_rate / 10) * 1.4);
+
+        stream.on('data', function(data) {
+          //push data to all connected clients
+          globals.clients.forEach(function(client) {
+            console.log('writing data for client');
+            client.write(data);
+          });
+        });
+
+        globals.currentStream = stream;
+
+        res.sendStatus(200).end();
+      });
+    },
+
+    end: function(callback) {
+      globals.currentStream.on('close', function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          callback();
+        }
+      });
+
+      globals.currentStream.destroy();
+    }
+  },
+
+  media: {
+    get: function(req, res) {
+      fs.readdir(__dirname + mediaDir, function(err, files) {
+        if (err) {
+          console.log(err);
+          throw (err);
+        } else {
+          var ret = [];
+          files.forEach(function(file) {
+            var fileObj = {};
+            fileObj.fileName = file;
+            ret.push(fileObj);
+          });
+          res.json(ret).end();
+        }
+      });
+    }
+  },
+
+  client: {
+    connect: function(req, res) {
+      res.writeHead(200, {
+        'Content-Type': 'audio/mpeg',
+        'Transfer-Encoding': 'chunked'
+      });
+
+      req.on("close", function() {
+        var index = globals.clients.indexOf(res);
+        globals.clients.splice(index, 1);
+        console.log('request closed - remove response from global array');
+        res.end();
+      });
+
+      globals.clients.push(res);
+
+      console.log('client connected - add response to global array');
+    }
+  }
+};
