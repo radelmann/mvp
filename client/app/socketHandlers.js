2ae6c328436c5624be8bed41d57f6fdcacd4ad7f
radelmann <robadelmann@gmail.com>
2016-01-05 16:58:11 -0800

    added listener count to dj playlist page

--- client/app/socketHandlers.js
+++ client/app/socketHandlers.js
@@ -1,7 +1,15 @@
 $(function() {
-    var socket = io();
+  var socket = io();
 
-    window.emitCurrentTrack = function(fileName) {
-      socket.emit('current-track', fileName);
-    }     
+  window.emitCurrentTrack = function(fileName) {
+    socket.emit('current-track', fileName);
+  }
+
+  socket.on('user joined', function(data) {
+      $('#listeners').text(data.numUsers);
+  });
+
+  socket.on('user left', function(data) {
+      $('#listeners').text(data.numUsers);
+  });
 });
