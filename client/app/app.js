172c91320cb3813fa961d08334e4815816e20d26
radelmann <robadelmann@gmail.com>
2016-01-19 19:42:14 -0800

    converted chatroom to angular view and controller

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,7 @@
 angular.module('liveStream', [
     'liveStream.services',
     'liveStream.playList',
+    'liveStream.listeningRoom',
     'ngRoute'
   ])
   .config(function($routeProvider, $httpProvider) {
@@ -8,6 +9,10 @@ angular.module('liveStream', [
       .when('/dj', {
         templateUrl: 'app/playList/playList.html',
         controller: 'playListController'
+      })
+      .when('/listen', {
+        templateUrl: 'app/listeningRoom/listeningRoom.html',
+        controller: 'listeningRoomController'
       })
       .otherwise('/dj');
   });
