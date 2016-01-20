2ecae9b063c7ec28173bc3e7a34263a1fc746d45
radelmann <robadelmann@gmail.com>
2016-01-19 17:12:18 -0800

    added socket.io angular service

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -14,7 +14,6 @@ angular.module('liveStream.services', [])
   };
 
   var play = function(track) {
-    console.log('ajax play' + track);
     return $http({
         method: 'POST',
         url: '/api/stream',
@@ -28,5 +27,31 @@ angular.module('liveStream.services', [])
   return {
     getAll: getAll,
     play: play
+  };
+})
+
+.factory('socket', function ($rootScope) {
+  var socket = io.connect();
+
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {  
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
   };
 });
